49b55b447153b4d90bcb91e2c3dba2e04ec2c946
Fabio Maiolo <fabio_maiolo@hotmail.com>
2020-04-14 22:11:22 -0300

    second commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,10 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question];
+    @coach = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"];
+  end
+end
